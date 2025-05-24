0ae6028f7d1e06bf06d24c234712b329bfa6e44a
Azlan19 <omarazlan6@gmail.com>
2025-05-24 23:22:34 +0200

    CLEAN - Cleaned main C01 ex07

--- projects/C01/ex07/ft_rev_int_tab.c
+++ projects/C01/ex07/ft_rev_int_tab.c
@@ -6,17 +6,15 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:47:10 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/15 17:31:31 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/24 23:22:14 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	temp;
 	int	i;
-
+	
 	i = 0;
 	while (i < size)
 	{
@@ -28,19 +26,31 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-// int temp_size;
-// temp_size = size;
-// i = 0;
-// while (i < temp_size)
-// {
-// 	printf("tab[%d] = %d\n", i, tab[i]);
-// 	i++;
-// }
-
+// #include <stdio.h>
 // int	main(void)
 // {
+// 	int i;
 // 	int	int_array[5] = {5, 6, 7, 8, 9};
-
+	
+// 	printf("\nBEFORE:\n");
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		printf("int_array[%d] = %d\n", i, int_array[i]);
+// 		i++;
+// 	}
+	
 // 	ft_rev_int_tab(int_array, 5);
+	
+// 	printf("\nAFTER:\n");
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		printf("int_array[%d] = %d\n", i, int_array[i]);
+// 		i++;
+// 	}
+	
+// 	printf("\n");
+	
 // 	return (0);
 // }
