a682f0ff1a1854e30e9dd8c5fce18ff018adee2a
Azlan19 <omarazlan6@gmail.com>
2025-05-24 22:54:48 +0200

    CLEAN - changed location of \n in the main of C01 ex02

--- projects/C01/ex02/ft_swap.c
+++ projects/C01/ex02/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:14:04 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/24 22:49:24 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/24 22:53:17 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	ft_swap(int *a, int *b)
 	
 // 	printf("\nBEFORE:\n");
 // 	printf("num1, %d, is stored in %p\n", num1, &num1);
-// 	printf("num2, %d, is stored in %p\n\n",num2, &num2);
+// 	printf("num2, %d, is stored in %p\n",num2, &num2);
 	
 // 	ft_swap(&num1, &num2);
 	
-// 	printf("AFTER:\n");
+// 	printf("\nAFTER:\n");
 // 	printf("num1, %d, is stored in %p\n",num1, &num1);
 // 	printf("num2, %d, is stored in %p\n",num2, &num2);
 	
