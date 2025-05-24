8a9340ba129b6d4f4543ad41439784353be83bd8
Azlan19 <omarazlan6@gmail.com>
2025-05-24 22:49:51 +0200

    CLEAN - Cleaned main C01 ex02

--- projects/C01/ex02/ft_swap.c
+++ projects/C01/ex02/ft_swap.c
@@ -6,28 +6,36 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:14:04 by oazlan            #+#    #+#             */
-/*   Updated: 2025/04/30 20:31:19 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/24 22:49:24 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
 	int	tmp;
-
+	
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
+// #include <stdio.h>
 // int	main()
 // {
 // 	int	num1 = 4;
 // 	int num2 = 3;
-// 	printf("\nBefore:\nnum1, %d, is stored in %p\n", num1, &num1);
+	
+// 	printf("\nBEFORE:\n");
+// 	printf("num1, %d, is stored in %p\n", num1, &num1);
 // 	printf("num2, %d, is stored in %p\n\n",num2, &num2);
+	
 // 	ft_swap(&num1, &num2);
-// 	printf("After:\nnum1, %d, is stored in %p\n",num1, &num1);
+	
+// 	printf("AFTER:\n");
+// 	printf("num1, %d, is stored in %p\n",num1, &num1);
 // 	printf("num2, %d, is stored in %p\n",num2, &num2);
+	
+// 	printf("\n");
+
+// 	return 0;
 // }
