38a170298eb545fdd5729cee8ebb811ff243abcd
Azlan19 <omarazlan6@gmail.com>
2025-05-24 23:30:08 +0200

    CLEAN - Renamed variable for array size C01 ex07

--- projects/C01/ex07/ft_rev_int_tab.c
+++ projects/C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:47:10 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/24 23:27:37 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/24 23:29:17 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ void	ft_rev_int_tab(int *tab, int size)
 // int	main(void)
 // {
 // 	int i;
-// 	int n = 5;
+// 	int array_size = 5;
 // 	int	int_array[] = {5, 6, 7, 8, 9};
 	
 // 	printf("\nBEFORE:\n");
 // 	i = 0;
-// 	while (i < n)
+// 	while (i < array_size)
 // 	{
 // 		printf("int_array[%d] = %d\n", i, int_array[i]);
 // 		i++;
@@ -45,7 +45,7 @@ void	ft_rev_int_tab(int *tab, int size)
 	
 // 	printf("\nAFTER:\n");
 // 	i = 0;
-// 	while (i < n)
+// 	while (i < array_size)
 // 	{
 // 		printf("int_array[%d] = %d\n", i, int_array[i]);
 // 		i++;
