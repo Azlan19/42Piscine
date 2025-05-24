ee2e357ecf180be2303d0c6cd0cff720f0f6d161
Azlan19 <omarazlan6@gmail.com>
2025-05-24 22:35:40 +0200

    CLEAN - Cleaned main C01 ex01

--- projects/C01/ex01/ft_ultimate_ft.c
+++ projects/C01/ex01/ft_ultimate_ft.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 00:09:00 by oazlan            #+#    #+#             */
-/*   Updated: 2025/04/30 20:14:48 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/24 22:35:10 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,26 @@ void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
+
+// #include <stdio.h>
+// int main ()
+// {
+// 	int x = 61;
+// 	int *p1 = &x;
+// 	int **p2 = &p1;
+// 	int ***p3 = &p2;
+// 	int ****p4 = &p3;
+// 	int *****p5 = &p4;
+// 	int ******p6 = &p5;
+// 	int *******p7 = &p6;
+// 	int ********p8 = &p7;
+// 	int *********p9 = &p8;
+	
+// 	printf("BEFORE: x = %d\n", x);
+	
+// 	ft_ultimate_ft(p9);
+	
+// 	printf("AFTER : x = %d\n", x);
+
+// 	return 0;
+// }
