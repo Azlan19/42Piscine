ecc65f7d90cb31284f5c668f24f03a9a44898310
Azlan19 <omarazlan6@gmail.com>
2025-05-25 21:20:08 +0200

    CLEAN - Cleaned main C02 ex01

--- projects/C02/ex01/ft_strncpy.c
+++ projects/C02/ex01/ft_strncpy.c
@@ -6,17 +6,14 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 11:56:01 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/01 17:29:21 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/25 21:19:53 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
-// #include <unistd.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-
+	
 	i = 0;
 	while (src[i] != '\0' && i < n)
 	{
@@ -31,34 +28,24 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-// void	ft_putstr(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		write(1, &str[i], 1);
-// 		i++;
-// 	}
-// }
-
+// #include <stdio.h>
 // int main()
 // {
-// 	char source[7] = "Copper";
-// 	char destination[9] = "";
+// 	char source[] = "Copper";
+// 	char destination[] = "Platinum";
+// 	int copy_size = 4;
 
-// 	printf("\nBefore:\n");
-// 	ft_putstr(source);
+// 	printf("\nBEFORE:\n");
+// 	printf("Source      = %s\n", source);
+// 	printf("Destination = %s\n", destination);
+	
+// 	ft_strncpy(destination, source, copy_size);
+	
+// 	printf("\nAFTER:\n");
+// 	printf("Source      = %s\n", source);
+// 	printf("Destination = %s\n", destination);
+	
 // 	printf("\n");
-// 	ft_putstr(destination);
-
-// 	ft_strncpy(destination, source, 9);
-
-// 	printf("\n\nAfter:\n");
-// 	ft_putstr(source);
-// 	printf("\n");
-// 	ft_putstr(destination);
-
+	
 // 	return 0;
 // }
