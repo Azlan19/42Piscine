8db4291e01a5047c9de3936dff8565097697c0bd
Azlan19 <omarazlan6@gmail.com>
2025-05-25 21:36:57 +0200

    CLEAN - Cleaned main C02 ex05

--- projects/C02/ex05/ft_str_is_uppercase.c
+++ projects/C02/ex05/ft_str_is_uppercase.c
@@ -6,11 +6,9 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 16:20:01 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/01 18:05:13 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/25 21:36:28 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
 
 int	ft_str_is_uppercase(char *str)
 {
@@ -37,9 +35,17 @@ int	ft_str_is_uppercase(char *str)
 	return (statement);
 }
 
+// #include <stdio.h>
 // int main()
 // {
-// 	char *word = "ASDaA";
-// 	ft_str_is_uppercase(word);
+// 	char *string_1 = "ABCDEFGHIJK";
+// 	char *string_2 = "ABCDEFGhIJK";
+	
+// 	printf("\n1 = TRUE, 0 = FALSE\n\n");
+// 	printf("'%s' is all uppercase? %d\n",string_1, ft_str_is_uppercase(string_1));
+// 	printf("'%s' is all uppercase? %d\n",string_2, ft_str_is_uppercase(string_2));
+	
+// 	printf("\n");
+	
 // 	return 0;
 // }
