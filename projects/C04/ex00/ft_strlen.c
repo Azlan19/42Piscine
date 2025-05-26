88b69f774402077ba809ee995b36c4475f4ec935
Azlan19 <omarazlan6@gmail.com>
2025-05-26 21:16:10 +0200

    CLEAN - Cleaned main C04 ex00

--- projects/C04/ex00/ft_strlen.c
+++ projects/C04/ex00/ft_strlen.c
@@ -6,16 +6,14 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 11:26:13 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/04 11:38:57 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/26 21:15:51 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
 
 int	ft_strlen(char *str)
 {
 	int	i;
-
+	
 	i = 0;
 	while (str[i] != '\0')
 	{
@@ -24,10 +22,14 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+// #include <stdio.h>
 // int main()
 // {
 // 	char *word = "whataaaaa";
-// 	printf("%d\n", ft_strlen(word));
+	
+	
+// 	printf("\nString = '%s'\n", word);
+// 	printf("\nString length = %d\n\n", ft_strlen(word));
 
 // 	return 0;
 // }
