fd72a55b933466ce13774cbf4d4c14b66441ded9
Azlan19 <omarazlan6@gmail.com>
2025-05-26 21:13:11 +0200

    CLEAN - Cleaned main C03 ex05

--- projects/C03/ex05/ft_strlcat.c
+++ projects/C03/ex05/ft_strlcat.c
@@ -6,31 +6,9 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 19:38:32 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/17 17:35:00 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/26 21:12:50 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*	The strlcat() functions copy and concatenate strings with the same input
-	parameters and output result as snprintf(3).
-
-	strlcat() takes the full size of the destination buffer and guarantees
-	NUL-termination if there is room. Note that room for the NUL should be
-	included in dstsize.
-
-	strlcat() appends string src to the end of dst. It will append at most
-	dstsize - strlen(dst) - 1 characters. It will then NUL-terminate, unless
-	dstsize is 0 or the original dst string was longer than dstsize (in practice
-	this should not happen as it means that either dstsize is incorrect or that
-	dst is not a proper string).
-
-	If the src and dst strings overlap, the behavior is undefined.
-
-	strlcat() functions return the total length of the string they tried to
-	create. For strlcat() that means the initial length of dst plus the length
-	of src.
-
-	If the return value is >= dstsize, the output string has been truncated. It
-	is the caller's responsibility to handle this.*/
 
 int	ft_strlen(char *str)
 {
@@ -69,12 +47,23 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	char	*word;
-// 	char	*dest;
+// 	char dest[30] = "Hello, ";
+// 	char src[] = "world!";
+// 	unsigned int size = sizeof(dest);
+// 	unsigned int result;
 
-// 	dest = "1337 42";
-// 	word = "Born to code";
-// 	printf("%d\n", ft_strlcat(dest, word, 5));
-// 	printf("%s\n", dest);
+// 	printf("\nBEFORE:\n");
+// 	printf("Source      = '%s'\n", src);
+// 	printf("Destination = '%s'\n", dest);
+// 	printf("Buffer size = %u\n", size);
+	
+// 	result = ft_strlcat(dest, src, size);
+	
+// 	printf("\nAFTER:\n");
+// 	printf("Destination = '%s'\n", dest);
+// 	printf("Total length attempted = %u\n", result);
+	
+// 	printf("\n");
+	
 // 	return (0);
 // }
