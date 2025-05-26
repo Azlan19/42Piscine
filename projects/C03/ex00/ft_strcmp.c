a680b4c9caef309ae7378cc98db395fa3a1cdb15
Azlan19 <omarazlan6@gmail.com>
2025-05-26 17:55:05 +0200

    CLEAN - Cleaned main C03 ex00

--- projects/C03/ex00/ft_strcmp.c
+++ projects/C03/ex00/ft_strcmp.c
@@ -6,16 +6,14 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 18:48:18 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/17 16:36:39 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/26 17:54:45 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-
+	
 	i = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
@@ -28,10 +26,13 @@ int	ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
+// #include <stdio.h>
 // int main()
 // {
 // 	char *word1 = "Hellz";
 // 	char *word2 = "Hella";
-// 	printf("%d\n", ft_strcmp(word1, word2));
+	
+// 	printf("\nThe difference between '%s' and '%s' is %d\n\n", word1, word2, ft_strcmp(word1, word2));
+	
 // 	return (0);
 // }
