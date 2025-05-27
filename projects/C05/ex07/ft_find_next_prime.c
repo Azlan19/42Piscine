a498bc93916a06bb0f210d09385abca888ddf3fb
Azlan19 <omarazlan6@gmail.com>
2025-05-27 16:29:51 +0200

    CLEAN - Cleaned main C05 ex07

--- projects/C05/ex07/ft_find_next_prime.c
+++ projects/C05/ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 13:26:51 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/11 13:36:00 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/27 16:29:32 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,17 @@ int	ft_find_next_prime(int nb)
 // #include <stdio.h>
 // int main()
 // {
-// 	printf("%d\n", ft_find_next_prime(800));
+// 	int num1 = 7;
+// 	int num2 = 50;
+// 	int num3 = 800;
+	
+// 	printf("\n\n");
+		
+// 	printf("The next prime number after %d is %d\n", num1, ft_find_next_prime(num1));
+// 	printf("The next prime number after %d is %d\n", num2, ft_find_next_prime(num2));
+// 	printf("The next prime number after %d is %d\n", num3, ft_find_next_prime(num3));
+	
+// 	printf("\n");
+	
 // 	return 0;
 // }
