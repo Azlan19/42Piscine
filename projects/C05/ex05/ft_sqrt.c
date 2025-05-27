b459440d19de030fac27537e27e43b392c0277b0
Azlan19 <omarazlan6@gmail.com>
2025-05-27 16:23:39 +0200

    CLEAN - Cleaned main C05 ex05

--- projects/C05/ex05/ft_sqrt.c
+++ projects/C05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 12:37:43 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/11 12:54:13 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/27 16:23:16 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,17 @@ int	ft_sqrt(int nb)
 // #include <stdio.h>
 // int main()
 // {
-
-// 	printf("%d\n", ft_sqrt(12));
+// 	int num1 = 12;
+// 	int num2 = 36;
+// 	int num3 = 81;
+	
+// 	printf("\nx = 0 if x is not a square number\n\n");
+	
+// 	printf("Square root of %d = %d\n", num1, ft_sqrt(num1));
+// 	printf("Square root of %d = %d\n", num2, ft_sqrt(num2));
+// 	printf("Square root of %d = %d\n", num3, ft_sqrt(num3));
+	
+// 	printf("\n");
+	
 // 	return 0;
 // }
