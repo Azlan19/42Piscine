1c53fa7b98c6c253094388c91d048fe9802e6471
Azlan19 <omarazlan6@gmail.com>
2025-05-28 13:17:39 +0200

    CLEAN - Cleaned main C07 ex01

--- projects/C07/ex01/ft_range.c
+++ projects/C07/ex01/ft_range.c
@@ -6,19 +6,18 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 17:11:35 by oazlan            #+#    #+#             */
-/*   Updated: 2025/05/14 13:26:29 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/05/28 13:17:23 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-// #include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
 	int	*ptr;
 	int	i;
 	int	size;
-
+	
 	if (max <= min)
 	{
 		return (0);
@@ -26,7 +25,7 @@ int	*ft_range(int min, int max)
 	size = max - min;
 	ptr = (int *) malloc(sizeof(int) * size);
 	if (ptr == NULL)
-		return (0);
+	return (0);
 	i = 0;
 	while (min < max)
 	{
@@ -36,10 +35,32 @@ int	*ft_range(int min, int max)
 	}
 	return (ptr);
 }
-// printf("%d\n", ptr[i]);
 
-// int main()
+// #include <stdio.h>
+// int	main(void)
 // {
-// 	ft_range(1, 18);
+// 	int min = 5;
+// 	int max = 10;
+// 	int *range;
+// 	int i;
+
+// 	range = ft_range(min, max);
+	
+// 	printf("\n");
+// 	if (range == NULL)
+// 	{
+// 		printf("ft_range returned NULL (invalid range or memory allocation failed).\n");
+// 		return (1);
+// 	}
+
+// 	printf("Range from %d to %d (exclusive):\n", min, max);
+// 	for (i = 0; i < max - min; i++)
+// 	{
+// 		printf("%d ", range[i]);
+// 	}
+// 	printf("\n\n");
+
+// 	free(range);
+	
 // 	return 0;
 // }
